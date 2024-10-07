2eaf32af60e5fd5be33feabb0cc45612229826ee
Ira <irynadubets7@gmail.com>
2024-10-07 17:09:51 +0300

    feat: add job functionality

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import CreateJobForm from "@/components/CreateJobForm";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 function AddJobPage() {
-  return <div>AddJobPage</div>;
+  const queryClient = new QueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <CreateJobForm />
+    </HydrationBoundary>
+  );
 }
 
 export default AddJobPage;
